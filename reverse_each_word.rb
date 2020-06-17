38ed0e88afdab7c697f88365c348ed5c8045a941
Nick Lanese <nlanese21@gmail.com>
2020-06-17 19:26:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(sentence)
   new_array.each do | word |
     return_array << word.reverse
   end
-  return_array.join(" ")
-  return return_array
+  return_string = return_array.join(" ")
+  return return_string
 end
